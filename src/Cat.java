ca03216e238702b110584e90157d95fd90acdb4d
JannaZZZ <w-e_like@mail.ru>
2022-01-17 17:32:14 +0400

    Firs commit

---
+++ src/Cat.java
@@ -0,0 +1,43 @@
+public class Cat {
+    double weight;
+    String name;
+    int age;
+    String color;
+
+    public Cat() {
+        weight = 3000;
+        name = "Barsik";
+        age = 1;
+        color = "Grey";
+    }
+
+    public Cat(double weight1, String name1, int age1, String color1) {
+        weight = weight1;
+        name = name1;
+        age = age1;
+        color = color1;
+    }
+    void Vivod(){
+        System.out.println("Имя: "  + name + " Вес: "  + weight + "г Возраст: "  + age + "год" + " Цвет: "  + color);
+    }
+    void pee(){
+        weight-= 100;
+    }
+    void okraska(String colorNew){
+        color = colorNew;
+    }
+    public static void main(String[] args) {
+        Cat Barsik = new Cat();
+        Cat bigCat = new Cat(5500, "Murka", 2, "Grey");
+        Cat Pupsik = new Cat(8000, "Barsik", 5, "Pink");
+        Pupsik.okraska("Red");
+        bigCat.okraska("Black");
+        Barsik.Vivod();
+        bigCat.Vivod();
+        Pupsik.Vivod();
+    }
+    }
+
+
+
+
